7d7e047e6dc5a4a12ee1d741c826aade5e157c62
Akash <akashyoungstar@gmail.com>
2024-12-24 13:50:57 +0530

    cors update

--- src/index.ts
+++ src/index.ts
@@ -36,8 +36,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://inventory-frontend-kappa-liart.vercel.app",
-      "https://inventory-frontend-4kqb7knr4-akashs-projects-848d32a6.vercel.app",
+      "https://taskmanager-frontend-6rmh.vercel.app",
     ],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
